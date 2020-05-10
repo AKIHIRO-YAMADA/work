dda6d8dfa087799f8364ea849a88270bd6775f9e
AKIHIRO-YAMADA <daa.xx.com.akki@gmail.com>
2020-05-10 22:15:38 +0900

    Finish4-11

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '848f9c8d28d7e833fb0baa0901a03cc05f1ad4cdbb3b33e18595c62210d91c1bc4dd1459baa3262a57d55717e86c976d6de609e98770a8e6c930349ebb3e2169'
